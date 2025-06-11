46c93b62cb103969a96f73245015b029afba534b
syed ali kazmi <syedalikazmi0012@gmail.com>
2025-06-11 10:33:47 +0500

    frontend updated 1

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,14 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:5173', process.env.FRONTEND_URL].filter(Boolean),
+  origin: [
+    'http://localhost:5173',
+    'https://karsal-azadari-network-mtds.vercel.app',
+    process.env.FRONTEND_URL
+  ].filter(Boolean),
   credentials: true,
 };
+
 
 app.use(cors(corsOptions));
 app.use(express.json());
